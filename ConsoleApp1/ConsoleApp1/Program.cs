2ad0659fae799d82077ce1c77194d6df03f3bed2
Виталик <Profilteh22@gmail.com>
2019-09-04 01:01:07 +0300

    First work

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,31 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Dima test 2");
+            int i = 0;
+            while (i != 999)
+            {
+                int count = i;
+                int x = 0;
+                int y = 0;
+                if (count >= 100)
+                {
+                    int buffer = count;
+                    x = buffer % 10;
+                    count = count / 10;
+                }
+                if (count >= 10)
+                {
+                    int buffer = count;
+                    y = buffer % 10;
+                    count = count / 10;
+                }
+                int e = count + x + y;
+                if (e == 15)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
         }
     }
 }
